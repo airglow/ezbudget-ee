3f8339485d7a3ae9ae8f2617e7f2297071e246ff
Mehdi <mehdirahimi@email.com>
2022-06-17 16:27:40 +0330

    Complete customer entity to DTO (and vice versa) conversion methods.

--- src/main/java/ir/ezbudget/dto/CustomerDto.java
+++ src/main/java/ir/ezbudget/dto/CustomerDto.java
@@ -31,6 +31,16 @@ public class CustomerDto {
         customer.setCode(customerDto.getCode());
         customer.setFirstName(customerDto.getFirstName());
         customer.setLastName(customerDto.getLastName());
+        customer.setPhoneNumber(customerDto.getPhoneNumber());
+        customer.setCellPhoneNumber(customerDto.getCellPhoneNumber());
+        customer.setFax(customerDto.getFax());
+        customer.setEmail(customerDto.getEmail());
+        customer.setAddress(customerDto.getAddress());
+        customer.setCreatedAt(new Date());
+        customer.setGender(customerDto.getGender());
+        customer.setStatus(customerDto.getStatus());
+        customer.setType(customerDto.getType());
+        customer.setCity(customerDto.getCity());
 
         return customer;
     }
@@ -40,6 +50,16 @@ public class CustomerDto {
         customerDto.setCode(customer.getCode());
         customerDto.setFirstName(customer.getFirstName());
         customerDto.setLastName(customer.getLastName());
+        customerDto.setPhoneNumber(customer.getPhoneNumber());
+        customerDto.setCellPhoneNumber(customer.getCellPhoneNumber());
+        customerDto.setFax(customer.getFax());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setAddress(customer.getAddress());
+        customerDto.setCreatedAt(customer.getCreatedAt());
+        customerDto.setGender(customer.getGender());
+        customerDto.setStatus(customer.getStatus());
+        customerDto.setType(customer.getType());
+        customerDto.setCity(customer.getCity());
 
         return customerDto;
     }
